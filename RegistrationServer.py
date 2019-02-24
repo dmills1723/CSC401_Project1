6084b6f4738a04e7d78ed1d622cadf5f7f56c41d
Jonathan Balliet <jrballiet@my.waketech.edu>
2019-02-23 23:52:37 -0500

    Updated RegistrationServer.py

--- RegistrationServer.py
+++ RegistrationServer.py
@@ -1,68 +1,197 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
+from PeerList import PeerList
+import ProtocolTranslator
 import socket
 import threading
 import time
+import sys
+import struct
 
 
+"""
+A thread that handles the tcp connection between a single Peer
+and the Registration Server.
+"""
 class PeerThread(threading.Thread):
     
+    """
+    Constructor for this tcp connection with the peer.
+    Sets the ip address, port number, and socket for 
+    this tcp connection.
+    """
     def __init__( self, ip_addr, port, socket ):
         threading.Thread.__init__(self)
         self.ip_addr = ip_addr
         self.port = port
         self.socket = socket
-        # peer list object
         
-        
+    """
+    Handles the request protocl sent from the peer and
+    sends back the approriate response protocol to this
+    peer over this tcp connection.
+    """
     def run(self):
         try:
             # bytes sent from the peer
-            data = self.socket.recv(2048)
-            # convert bytes to string
-            msg = str(data.decode('ascii'))
-            # Peer sends the Register command
-            if msg == "Register":
-                # do the register method, port number
-                # sends back cookie
-                # Peer sends the Leave command
-                return
-            elif msg == "Leave":
-                # always check if this peer exists first before processing other requests
-                # sets flag in peer list to false
-            # Peer sends the PQuery command
-                return
-            elif msg == "PQuery":
-                # sends back a list of active peers that includes
-                # the hostname and RFC server port information.
-            # Peer sends the KeepAlive command
-                return
-            elif data == "KeepAlive":
-                # upon receipt of this message, the RS resets the TTL value for this peer to 7200.
-                return
-            # Else invalid command
+            request_bytes = self.socket.recv(2048)
+            
+            # convert the request in bytes to string
+            request = str( request_bytes.decode('ascii') )
+            
+            print(request)
+            
+            # obtains the method of this protocol
+            method = request.splitlines()[0]
+    
+            # Peer sends the Register request
+            if method == "Register":
+                # obtains the host, port, and cookie from the register request protocol
+                host, port, c = ProtocolTranslator.registerQueryToElements( request )
+                
+                # acquire lock on the peer list before modifying
+                lock.acquire()
+                
+                # registers this host in the peer list and obtains the cookie
+                cookie = peer_list.register( host, port, c )
+                
+                # release lock on the peer list after modifying
+                lock.release()
+                
+                # sends back a response message with the cookie
+                response = ProtocolTranslator.registerResponseToProtocol( cookie )
+                
+                print(response)
+                
+                # translates the response protocol into bytes
+                response_bytes = response.encode('ascii')
+                
+                # sends the response to the peer client
+                self.socket.send( response_bytes )
+                
+            # Peer sends the Leave request  
+            elif method == "Leave":
+                
+                # obtains the cookie for this peer from the request
+                cookie = ProtocolTranslator.leaveQueryToElements( request )
+                
+                # acquires lock on the peer list before modifying
+                lock.acquire()
+                
+                # attempts to have the peer leave if there is matching peer in the peerlist with this cookie
+                # returns a boolean value of whether the peer successfully could leave
+                peer_left = peer_list.leave( cookie )
+                
+                # releases lock on the peer list after modifying
+                lock.release()
+                
+                # creates the protocol response to be sent back to the peer
+                response = ProtocolTranslator.leaveResponseToProtocol( peer_left )
+                
+                print(response)
+                
+                # translates the response protocol into bytes
+                response_bytes = response.encode('ascii')
+                
+                # sends the response to the peer client
+                self.socket.send( response_bytes )
+                
+                
+            # Peer sends the PQuery request      
+            elif method == "PQuery":
+            
+                # obtains the cookie for this peer from the request
+                cookie = ProtocolTranslator.pqueryQueryToElements( request )
+                
+                # acquires lock on the peer list before modifying
+                lock.acquire()
+                
+                # attempts to find the matching peer in this peer list based on the cookie
+                # returns the peerlist of active peers or None if the peer hasn't registered yet
+                p_list = peer_list.pQuery( cookie )
+                                
+                # releases lock on the peer list after modifying
+                lock.release()
+                
+                can_query = False
+                p_list_str = ''
+                if p_list is not None:
+                    can_query = True
+                    p_list = PeerList(p_list)
+                    p_list_str = str(p_list)
+     
+                response = ProtocolTranslator.pqueryResponseToProtocol( can_query, p_list_str )
+                
+                print(response)
+     
+                # translates the response protocol into bytes
+                response_bytes = response.encode('ascii')
+                
+                self.socket.send( response_bytes )
+                    
+            # Peer sends the KeepAlive request
+            elif method == "KeepAlive":
+                
+                # obtains the cookie for this peer from the request
+                cookie = ProtocolTranslator.keepAliveQueryToElements( request )
+                
+                # acquires lock on the peer list before modifying
+                lock.acquire()
+                
+                # attempts to find the matching peer in this peer list based on the cookie
+                # returns the a status code value if this was successful
+                status_code = peer_list.keepAlive( cookie )
+                
+                # releases lock on the peer list after modifying
+                lock.release()
+                
+                # sets boolean value of whether this peer could keepAlive to false
+                can_keep_alive = False
+                
+                # TODO: might need to handle request differently depending on which invalid code (0 or 3)
+                # if the keepAlive request was valid - set the boolean value to true
+                if status_code == 1:
+                    can_keep_alive = True
+                
+                # creates the protocol response to be sent back to the peer
+                response = ProtocolTranslator.keepAliveResponseToProtocol( can_keep_alive )
+                
+                print(response)
+                
+                # translates the response protocol into bytes
+                response_bytes = response.encode('ascii')
+                
+                # sends the response to the peer client
+                self.socket.send( response_bytes )
+                
+            # Invalid request
             else:
-                #raise error('Error Processing Client request') 
-                self.socket.close()
-                return False                
+                # creates a response containing "BAD REQUEST" status code
+                response = ProtocolTranslator.leaveResponseToProtocol( False )
+                
+                print(response)
+                
+                # translates the response protocol into bytes
+                response_bytes = response.encode('ascii')
+                
+                # sends the response to the peer client
+                self.socket.send( response_bytes )
+        
         # Exception occured (or timeout?) close tcp connection and return false                
         except:
+            lock.release()
             self.socket.close()
-            return False
+            sys.exit()
+            
         # Always close tcp connection socket before thread returns
         self.socket.close()
         
-    def register(port_num):
-        return None
-        
-    def leave():
-        return None
-    
-    def peer_query():
-        return None
-    
-    def keep_alive():
-        return None
+
+# initalize the list of peers
+peer_list = PeerList()
+
+# create a lock for the threads
+lock = threading.Lock()
 
 # using local for testing      
 host = '127.0.0.1'
@@ -83,35 +212,35 @@ peer_threads = []
 # while the server is running
 while True:
 
-    start_time = time.time()
+    #start_time = time.time()
     
     # listen for new connections from peers
     sock.listen()
     
     # accept the connection from the client
-    # retrieve the client socket, ip address, and port number
+    # retrieve the client socket, ip address, and port number sent
     (client_sock, (ip_addr, port)) = sock.accept()
     
-    # obtain the new peer thread 
+    # obtain the new peer thread passing in the ip address, port number, and socket
     peer_thread = PeerThread(ip_addr, port, client_sock)
     
     # start this new peer thread
     peer_thread.start()
     
-    # append this peer thread to the list of peers
+    # append this peer thread to the list of threads
     peer_threads.append(peer_thread)
        
     # Q: where to decrement and check the TTL for all peers?
     # minus all TTL fields by amount of time passed (if peer is active)
     # if TTL is 0 or less -> change flag to inactive for the peer
     # might want a timeout for tcp connection to prevent blocking?
-    elapsed_time = time.time() - start_time
-    checkTTL(elapsed_time)
+    #elapsed_time = time.time() - start_time
+    #checkTTL(elapsed_time)
     
 # terminate each thread in the list of peer threads
 for thread in peer_threads:
     thread.join()
     
     
-def checkTTL( elapsed_time ):
-    return None
+#def checkTTL( elapsed_time ):
+    # iterate thorugh peer list and check TTL
